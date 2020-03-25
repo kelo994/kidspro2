88ac061ff0bf2264118944d42ddc099504b1a43f
Ezequiel <eze.monsal@gmail.com>
2020-03-24 22:01:57 -0300

    conectar select reportes objetivos

--- src/app/components/pages/reportes/objetivos/objetivos.component.ts
+++ src/app/components/pages/reportes/objetivos/objetivos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { HighchartsService } from '../../../../services/highcharts.service';
 import { Router } from '@angular/router';
 
@@ -6,6 +6,8 @@ import { ChartDataSets, ChartType, ChartOptions } from 'chart.js';
 import { Color, BaseChartDirective, Label } from 'ng2-charts';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 import * as pluginAnnotations from 'chartjs-plugin-annotation';
+import { ReporteService } from '../../../../services/reporte.service';
+import { NzNotificationService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-objetivos',
@@ -13,17 +15,129 @@ import * as pluginAnnotations from 'chartjs-plugin-annotation';
   styleUrls: ['./objetivos.component.scss']
 })
 export class ObjetivosComponent implements OnInit {
+  @Input() selectCurso: any;
+  @Input() selectAsignatura: any;
 
-  constructor(private highcharts: HighchartsService, public router: Router) { }
+  unidades = [];
+  selectUnidad;
+  lecciones = [];
+  selectLeccion;
+  objetivos = [];
+  selectObjetivo;
+
+  estudiantes = [];
+  selectEstudiante;
+
+  curso = {
+    curso_nombre: 'Seleccione un Curso',
+    id: 0
+  }
+
+  asignatura = {
+    materia_descripcion: 'Seleccione Asignatura',
+    asignatura_id: 0
+  }
+
+  constructor(private highcharts: HighchartsService, public router: Router,
+    private notification: NzNotificationService, public rService: ReporteService) { }
 
   @ViewChild('analisisComparativo') public analisisComparativo: ElementRef;
 
   ngOnInit(): void {
+    // obtener Cursos
   }
 
-  ngAfterViewInit() {
+  ngOnChanges(changes: any) {
+    console.log(changes);
+    if(changes.selectCurso) {
+      if (changes.selectCurso.currentValue.curso_nombre) this.curso.curso_nombre = changes.selectCurso.currentValue.curso_nombre;
+      if (changes.selectCurso.currentValue.id) this.curso.id = changes.selectCurso.currentValue.id;
+    } else if(!(Number(this.curso.id) > 0)) {
+      this.notification.warning('Reporte de Objetivos', 'Error, no se ha seleccionado un curso.');
+    }
+    
+    if(changes.selectAsignatura) {
+      if (changes.selectAsignatura.currentValue.materia_descripcion) this.asignatura.materia_descripcion = changes.selectAsignatura.currentValue.materia_descripcion;
+    if (changes.selectAsignatura.currentValue.asignatura_id) this.asignatura.asignatura_id = changes.selectAsignatura.currentValue.asignatura_id;
+    } else if(!(Number(this.selectAsignatura.asignatura_id) > 0)) {
+      this.notification.warning('Reporte de Objetivos', 'Error, no se ha seleccionado una asignatura.');
+    }    
 
+    if (this.curso.id > 0 && this.asignatura.asignatura_id > 0) {
+      this.getDataAsignatura();
+      this.getEstudiantes();
+    }
   }
+
+  getDataAsignatura() {
+    this.rService.getDataAsignatura(this.curso.id, this.asignatura.asignatura_id).subscribe(
+      (data: any) => { // Success
+        console.log(data);
+        this.unidades = data;
+        if (this.unidades.length > 0) {
+          this.selectUnidad = this.unidades[0];
+          this.changeUnidad(this.selectUnidad);
+        }
+      },
+      (error) => {
+        console.log(error)
+        if (error.status == 401) {
+          this.router.navigate(['/auth/login']);
+        } else if (error.status == 400) {
+          this.router.navigate(['/auth/login']);
+        }
+      }
+    );
+  }
+
+  changeUnidad(itemU) {
+    this.lecciones = itemU.bloques;
+    this.selectLeccion = this.lecciones[0];
+    this.changeObjetivos(this.selectLeccion);
+  }
+
+  changeObjetivos(itemL) {
+    this.objetivos = itemL.objetivos;
+    this.selectObjetivo = this.objetivos[0];
+  }
+  
+  getEstudiantes() {
+    this.rService.getEstudiantesCurso(this.selectCurso.id).subscribe(
+      (data: any) => { // Success
+        console.log(data);
+        this.estudiantes = data;
+      },
+      (error) => {
+        console.log(error)
+        if (error.status == 401) {
+          this.router.navigate(['/auth/login']);
+        } else if (error.status == 400) {
+          this.router.navigate(['/auth/login']);
+        }
+      }
+    );
+  }
+
+  onChangeUnidad(item) {
+    this.lecciones = item.bloques;
+    this.selectLeccion = this.lecciones[0]
+    this.onChangeLeccion(this.selectLeccion);
+  }
+
+  onChangeLeccion(item) {
+    this.objetivos = item.objetivos;
+    this.selectObjetivo = this.objetivos[0];
+    this.onChangeObjetivo(this.selectObjetivo);
+  }
+
+  onChangeObjetivo(item) {
+    // render graficos
+  }
+
+  onChangeEstudiante(item) {
+    // render graficos
+  }
+  
 
   // lineChart
   public lineChartData: ChartDataSets[] = [
---
+++ src/app/services/reporte.service.ts
@@ -0,0 +1,83 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
+import { finalize } from 'rxjs/operators';
+import { timeout } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { retry } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ReporteService {
+
+    private url = `${environment.apiBaseUrl}`;
+
+    constructor(private http: HttpClient, public router: Router) { }
+
+    getToken() {
+        return {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                Authorization: localStorage.getItem('token')
+            })
+        };
+    }
+
+    // Curso
+    getACursosGraficos(rol, idx) {
+        let urlGraf = '';
+        if (rol == 2) {
+            urlGraf = `establecimientos/${idx}/cursos`;
+        } else {
+            urlGraf = `funcionarios/${idx}/cursos`;
+        }
+        return this.http.get(`${this.url}/${urlGraf}`, this.getToken()).pipe(timeout(5000),
+            retry(3),
+            catchError((error, c) => {
+                this.errorTime();
+                return throwError(error)
+            }),
+            switchMap(f => { /*console.log('do something with '+JSON.stringify(f));*/ return of(f) }),
+            finalize(() => { /*console.log('finilize')*/ }));;
+    }
+
+    // Asignaturas
+    getAsignaturasCurso(curso_id) {
+        return this.http.get(`${this.url}/cursos/${curso_id}/asignaturas`, this.getToken()).pipe(timeout(5000),
+            retry(3),
+            catchError((error, c) => {
+                this.errorTime();
+                return throwError(error)
+            }),
+            switchMap(f => { /*console.log('do something with '+JSON.stringify(f));*/ return of(f) }),
+            finalize(() => { /*console.log('finilize')*/ }));;
+    }
+
+    //obtiene el contenido de una asignatura (unidades, bloques, objetivos, habilidades)
+    getDataAsignatura(idCurso: any, idAsignatura: any) {
+        return this.http.get(`${this.url}/detalleAsignatura/${idCurso}/${idAsignatura}`, this.getToken());
+    }
+
+    // obtener estudiantes de un curso
+    getEstudiantesCurso(idx) {
+        return this.http.get(`${this.url}/cursos/especificos/${idx}/estudiantes`, this.getToken()).pipe(timeout(5000),
+            retry(3),
+            catchError((error, c) => {
+                this.errorTime();
+                return throwError(error)
+            }),
+            switchMap(f => { /*console.log('do something with '+JSON.stringify(f));*/ return of(f) }),
+            finalize(() => { /*console.log('finilize')*/ }));;
+    }
+
+    errorTime() {
+        // this.toast.eternalToast('danger', 'Error Inesperado', 'Lo sentimos, no se ha podido realizar la solicitud, intentelo más tarde');
+    }
+
+}
